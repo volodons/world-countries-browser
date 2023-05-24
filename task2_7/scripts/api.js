2e8df0fa95a8b4b5029cb9e0f07918fffd9717ee
Volodymyr Donets <volodons@gmail.com>
2023-05-24 12:11:29 +0300

    Add error handling for api requests

--- task2_7/scripts/api.js
+++ task2_7/scripts/api.js
@@ -5,30 +5,61 @@ function getAllCountriesData() {
     "https://restcountries.com/v3.1/all?fields=flags,name,population,region,capital";
   fetch(apiUrl)
     .then((response) => {
-      return response.json();
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error();
+      }
     })
     .then((data) => renderItems(data))
-    .catch((error) => console.log("Error:", error));
+    .catch((error) => {
+      console.log(error);
+      const itemsList = document.querySelector(".cards-wrapper");
+      itemsList.innerHTML = "";
+      const errorMessage = document.createElement("strong");
+      errorMessage.innerText = "Error: reload your page or try again later";
+      itemsList.append(errorMessage);
+    });
 }
 
 function getRegionDataByName(name) {
   const apiUrl = `https://restcountries.com/v3.1/region/${name}?fields=flags,name,population,region,capital`;
   fetch(apiUrl)
     .then((response) => {
-      return response.json();
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error();
+      }
     })
     .then((data) => renderItems(data))
-    .catch((error) => console.log("Error", error));
+    .catch((error) => {
+      console.log(error);
+      const itemsList = document.querySelector(".cards-wrapper");
+      itemsList.innerHTML = "";
+      const errorMessage = document.createElement("strong");
+      itemsList.append(errorMessage);
+    });
 }
 
 function getCountryDataByName(name) {
   const apiUrl = `https://restcountries.com/v3.1/name/${name}?fields=flags,name,population,region,capital`;
   fetch(apiUrl)
     .then((response) => {
-      return response.json();
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error();
+      }
     })
     .then((data) => renderItems(data))
-    .catch((error) => console.log("Error", error));
+    .catch((error) => {
+      console.log(error);
+      const itemsList = document.querySelector(".cards-wrapper");
+      itemsList.innerHTML = "";
+      const errorMessage = document.createElement("strong");
+      itemsList.append(errorMessage);
+    });
 }
 
 export { getAllCountriesData, getCountryDataByName, getRegionDataByName };
