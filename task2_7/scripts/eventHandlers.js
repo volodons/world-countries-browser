a245e09864dfce9f710e36c70f04015d3b33f01a
Volodymyr Donets <volodons@gmail.com>
2023-05-25 19:13:02 +0300

    Add debouncer + .then to api function calls

--- task2_7/scripts/eventHandlers.js
+++ task2_7/scripts/eventHandlers.js
@@ -4,26 +4,23 @@ import {
   getRegionDataByName,
 } from "./api.js";
 
+import { renderItems } from "./ui.js";
+
 // Get user's text input
 function getRegionName(event) {
   const formData = new FormData(event.currentTarget);
   const regionName = formData.get("searchByRegion");
-  getRegionDataByName(regionName);
+  getRegionDataByName(regionName).then(renderItems);
 }
-
-let timeoutId;
 
 // Get user's select input
 function getCountryName(event) {
-  clearTimeout(timeoutId);
-  timeoutId = setTimeout(() => {
-    const formData = new FormData(event.currentTarget);
-    const countryName = formData.get("searchByName");
-    if (!/^\s*$/.test(countryName)) {
-      getCountryDataByName(countryName);
-    }
-    getAllCountriesData();
-  }, 1000);
+  const formData = new FormData(event.currentTarget);
+  const countryName = formData.get("searchByName");
+  if (!/^\s*$/.test(countryName)) {
+    getCountryDataByName(countryName);
+  }
+  getAllCountriesData();
 }
 
 export { getRegionName, getCountryName };
--- task2_7/scripts/main.js
+++ task2_7/scripts/main.js
@@ -1,16 +1,30 @@
 import { getRegionName, getCountryName } from "./eventHandlers.js";
 import { getAllCountriesData } from "./api.js";
-import { toggleThemeColor } from "./ui.js";
+import { renderItems, toggleThemeColor } from "./ui.js";
 
 // Select forms and button
 const themeColorButton = document.querySelector(".button");
 const textForm = document.querySelector("#text-form");
 const selectForm = document.querySelector("#select-form");
 
+const debouncedGetCountryName = debounce(getCountryName, 1000);
+
 // Attach event listeners to forms and button
 themeColorButton.addEventListener("click", toggleThemeColor);
-textForm.addEventListener("input", getCountryName);
+textForm.addEventListener("input", debouncedGetCountryName);
 selectForm.addEventListener("input", getRegionName);
 
+// Add debounce
+function debounce(func, delay) {
+  let timerId;
+  return function (...args) {
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
+
 // Initial call of function to get all countries data from API
-getAllCountriesData();
+getAllCountriesData().then(renderItems(items));
